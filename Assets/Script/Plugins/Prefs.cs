3452e854b93b3044f1454786d148e1c0e2ed38ee
Hansjürg Jaggi <hanjo77@gmail.com>
2016-08-01 16:23:45 +0200

    Added Prefs to keep selection of voice.

---
+++ Assets/Script/Plugins/Prefs.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class Prefs {
+
+	public string voice;
+
+	public void Load()
+	{       
+		voice = PlayerPrefs.GetString("voice", "Otoya"); 
+	}
+
+	public void Save()
+	{       
+		PlayerPrefs.SetString("voice", voice); 
+	}
+
+	public void SetAll(ref GameBehaviour game)
+	{       
+		SetVoice(ref game);
+	}
+	public void SetVoice(ref GameBehaviour game)
+	{       
+		game.soundVoice = voice;
+	} 
+}
