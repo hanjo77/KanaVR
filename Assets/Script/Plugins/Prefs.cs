9cdc5fd0f7adcc3f9ecfedcc563570199dc7fac3
Hansjürg Jaggi <hanjo77@gmail.com>
2016-08-05 03:06:30 +0200

    Added touch controls.

--- Assets/Script/Plugins/Prefs.cs
+++ Assets/Script/Plugins/Prefs.cs
@@ -2,10 +2,12 @@
 public class Prefs {
 
 	public string voice;
+	public bool isVr;
 
 	public void Load()
 	{       
 		voice = PlayerPrefs.GetString("voice", "Otoya"); 
+		isVr = false; // (PlayerPrefs.GetInt ("vr", 0) > 0) ? true : false;
 	}
 
 	public void Save()
@@ -16,9 +18,18 @@ public class Prefs {
 	public void SetAll(ref GameBehaviour game)
 	{       
 		SetVoice(ref game);
+		SetVr (ref game);
 	}
 	public void SetVoice(ref GameBehaviour game)
 	{       
 		game.soundVoice = voice;
+	} 
+	public void SetVr(ref GameBehaviour game)
+	{       
+		GvrViewer viewer = GameObject.FindObjectOfType<GvrViewer> ();
+		if (viewer != null) {
+			viewer.VRModeEnabled = isVr;
+			viewer.GetComponentInChildren<GvrHead> ().trackRotation = isVr;
+		}
 	} 
 }
